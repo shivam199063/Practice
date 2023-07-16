66a81bf49d416bd42af07132b0fdc5721bbca548
shivam199063 <shivam199063@gmail.com>
2023-07-16 13:50:23 +0530

    LinkedinApi

--- openai/langchain1.py
+++ openai/langchain1.py
@@ -1,6 +1,6 @@
 
 from langchain.llms import OpenAI
-my_openai_key = "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"
+my_openai_key = "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"
 mymodel = OpenAI(temperature=0 ,openai_api_key=my_openai_key)
 
 
@@ -26,6 +26,29 @@ mychain = LLMChain(
     llm=mymodel
 )
 
-print(mychain.run(things="food"))
+# print(mychain.run(things="food"))
+
+
+# ***********************************************************************************************
+
+import os
+os.environ["OPENAI_API_KEY"] = '50898974cf5534c59add384df3a25fc0c7927c747952394f3e93a74da47817f0'
+
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.agents import load_tools
+
+mytool = load_tools(tool_names=['serpapi'])
+
+
+
+mygoogle_chain = initialize_agent(
+    tools=mytool,
+    llm = mymodel,
+    agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose = True
+    )
+
+mygoogle_chain.run("who is vimal in two points")
 
 
