fba9b04699538b4ef25d8b66ae763d6004922366
shivam199063 <shivam199063@gmail.com>
2023-07-18 00:41:20 +0530

    jenkins  update

--- practice/python/1.py
+++ practice/python/1.py
@@ -6,12 +6,19 @@
 # import sys
 # print(sys.executable)
 
-import threading
+# import threading
 
-def s():
-    print("shivam")
+# def s():
+#     print("shivam")
     
-def ss():
-    print("saini")
+# def ss():
+#     print("saini")
 
+import sys
+
+a =['10']
+b=a
+# c=a
+
+print(sys.getrefcount(b))
 
--- python/jenkins.py
+++ python/jenkins.py
@@ -1,4 +1,4 @@
-!#/usr/bin/python3
+#!/usr/bin/python3
 
 
 print("this is first github integration with jenkins")
