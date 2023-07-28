102ffa80ae0c09d74a26c7e4f8d71cfdd32f0b8e
shivam199063 <shivam199063@gmail.com>
2023-07-28 15:14:34 +0530

    update

--- python/dictionary.py
+++ python/dictionary.py
@@ -6,6 +6,9 @@ data = {
     "Remarks": ["ok","done","good","good"]
 }
 
+# Dictnory can't support Row wise operation So we convert it into dataFrame first
+
+
 data=pd.DataFrame(data)
 
 print(data.iloc[1:3]) #index location >> iloc
