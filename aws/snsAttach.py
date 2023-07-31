ccf6d5c165511089bb450032ef90892c6b0a7a8c
shivam199063 <shivam199063@gmail.com>
2023-08-01 00:09:16 +0530

    face detection and send email to admini

--- aws/snsAttach.py
+++ aws/snsAttach.py
@@ -1,19 +1,63 @@
+# import boto3
+
+# snsAlert = boto3.client("sns")
+
+# login= int(input("if alert press :1 \n if Successfull press: 2"))
+
+# if login ==1:
+#     snsAlert.publish(
+#        Message= "Anuthorized login....",
+#        Subject= "Malicious Threat Actor Detected",
+#        TopicArn= "arn:aws:sns:ap-south-2:789757563817:Alert"
+# )
+
+# if login ==2:
+#     snsAlert.publish(
+#        Message= "Authorized login....",
+#        Subject= "User login successfully...",
+#        TopicArn= "arn:aws:sns:ap-south-2:789757563817:Alert"
+# )
+
+
+
+
 import boto3
 
+# Initialize the SNS client
 snsAlert = boto3.client("sns")
 
-login= int(input("if alert press :1 \n if Successfull press: 2"))
+def check_credentials(user_id, password):
+    # Dictionary containing user ID-password pairs (Replace with your own data)
+    credentials = {
+        'user1': 'password1',
+        'user2': 'password2',
+        'user3': 'password3'
+    }
 
-if login ==1:
-    snsAlert.publish(
-       Message= "Anuthorized login....",
-       Subject= "Malicious Threat Actor Detected",
-       TopicArn= "arn:aws:sns:ap-south-2:789757563817:Alert"
-)
+    # Check if the user ID exists in the credentials dictionary
+    if user_id in credentials:
+        # If the provided password matches the password associated with the user ID
+        if password == credentials[user_id]:
+            return True
+    return False
 
-if login ==2:
+def send_alert(message, subject):
+    # Replace the TopicArn with your own SNS topic ARN
     snsAlert.publish(
-       Message= "Authorized login....",
-       Subject= "User login successfully...",
-       TopicArn= "arn:aws:sns:ap-south-2:789757563817:Alert"
-)
+        Message=message,
+        Subject=subject,
+        TopicArn="arn:aws:sns:ap-south-2:789757563817:Alert"
+    )
+def main():
+    user_id = input("Enter your user ID: ")
+    password = input("Enter your password: ")
+
+    if check_credentials(user_id, password):
+        print("Access granted!")
+    else:
+        print("Access denied!")
+        # Send an alert to the admin
+        send_alert("Unauthorized login attempt", "Malicious Threat Actor Detected")
+
+if __name__ == "__main__":
+    main()
